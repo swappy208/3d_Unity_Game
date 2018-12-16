d1b9bd6c728e3a792f5388e5c253cd3c8644ff66
Swapnil Tamrakar <swappy208@gmail.com>
2018-12-16 16:41:24 -0500

    Mouse Clicks

---
+++ Assets/Scripts/MouseClicks.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClicks : MonoBehaviour 
+{
+    private Color originalColor = Color.black;
+    private void Awake()
+    {
+        this.originalColor = this.GetComponent<Renderer>().material.color;
+    }
+    private void OnMouseEnter()
+    {
+        this.GetComponent<Renderer>().material.color = Color.red;
+    }
+    private void OnMouseExit()
+    {
+        this.GetComponent<Renderer>().material.color = originalColor;
+    }
+
+}
